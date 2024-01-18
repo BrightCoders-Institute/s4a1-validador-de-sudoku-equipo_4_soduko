72d8b0de21c90e246ff72640d3a96654f402adca
angel23v <angelvazquezvirgen@gmail.com>
2024-01-17 18:12:25 -0600

    Done test
    Tested all the posbile events
    
    Co-authored-by: Juan Blas <jjosueblas@gmail.com>
    Co-authored-by: Sharon Belmont <belmontsharon77@gmail.com>

---
+++ test.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: false
+
+require 'minitest/autorun'
+require_relative 'main'
+
+class TestSoduko < Minitest::Test
+  def setup
+    @tablero = [
+      [5, 3, 0, 0, 7, 0, 0, 0, 0],
+      [6, 0, 0, 1, 9, 5, 0, 0, 0],
+      [0, 9, 8, 0, 0, 0, 0, 6, 0],
+      [8, 0, 0, 0, 6, 0, 0, 0, 3],
+      [4, 0, 0, 8, 0, 3, 0, 0, 1],
+      [7, 0, 0, 0, 2, 0, 0, 0, 6],
+      [0, 6, 0, 0, 0, 0, 2, 8, 0],
+      [0, 0, 0, 4, 1, 9, 0, 0, 5],
+      [0, 0, 0, 0, 8, 0, 0, 7, 9]
+    ]
+    @tablero_not = [
+      [5, 3, 0, 0, 7, 0, 0, 0, 0],
+      [6, 0, 0, 1, 9, 5, 0, 0, 0],
+      [0, 9, 8, 0, 0, 0, 0, 6, 0],
+      [8, 0, 0, 0, 6, 0, 0, 0, 3],
+      [4, 0, 0, 8, 0, 3, 0, 0, 1],
+      [7, 0, 0, 0, 2, 0, 0, 0, 6],
+      [0, 6, 0, 0, 0, 0, 2, 8, 0],
+      [0, 0, 0, 4, 1, 9, 0, 0, 5],
+      [9, 0, 0, 0, 8, 0, 0, 7, 9]
+    ]
+  end
+
+  def test_valid_game
+    soduko_instance = Soduko.new(@tablero)
+    assert_equal true, soduko_instance.check_print
+  end
+
+  def test_not_valid_game
+    soduko_instance = Soduko.new(@tablero_not)
+    assert_equal false, soduko_instance.check_print
+  end
+end
